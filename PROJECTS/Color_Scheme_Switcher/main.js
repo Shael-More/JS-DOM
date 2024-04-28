dbac017e02f8c27148a6936dc9ce5953fd4ebad9
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-04-28 05:34:04 +0000

    if else/ switch

--- PROJECTS/Color_Scheme_Switcher/main.js
+++ PROJECTS/Color_Scheme_Switcher/main.js
@@ -1,24 +1,52 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 
-buttons.forEach(function(button) {
-    button.addEventListener("click", function(e) {
-        if (e.target.id === "grey") {
-            body.style.backgroundColor = e.target.id;
-            body.style.color = "red"
-        }
-        if (e.target.id === "white") {
-            body.style.backgroundColor = e.target.id;
-            body.style.color = "black"
-        }
-        if (e.target.id === "blue") {
-            body.style.backgroundColor = e.target.id;
-            body.style.color = "white"
-        }
-        if (e.target.id === "yellow") {
-            body.style.backgroundColor = e.target.id;
-            body.style.color = "green"
-        }
-    })
-})
+
+// // using if else statements
+// buttons.forEach(function(button) {
+//     button.addEventListener("click", function(e) {
+//         if (e.target.id === "grey") {
+//             body.style.backgroundColor = e.target.id;
+//             body.style.color = "red"
+//         }
+//         if (e.target.id === "white") {
+//             body.style.backgroundColor = e.target.id;
+//             body.style.color = "black"
+//         }
+//         if (e.target.id === "blue") {
+//             body.style.backgroundColor = e.target.id;
+//             body.style.color = "white"
+//         }
+//         if (e.target.id === "yellow") {
+//             body.style.backgroundColor = e.target.id;
+//             body.style.color = "green"
+//         }
+//     })
+// })
+
+
+// using switch statements
+// buttons.forEach(function(button) {
+//     button.addEventListener("click", function (e) {
+//         switch (e.target.id) {
+//             case "grey":
+//                 body.style.backgroundColor = e.target.id
+//                 break;
+//             case "white":
+//                 body.style.backgroundColor = e.target.id
+//                 break;
+//             case "blue":
+//                 body.style.backgroundColor = e.target.id
+//                 break;
+//             case "yellow":
+//                 body.style.backgroundColor = e.target.id
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+//     })
+// })
+
+
 
