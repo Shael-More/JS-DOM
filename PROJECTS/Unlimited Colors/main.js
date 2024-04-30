f10e82e9736e3907330ad30ed93ee370ee092da3
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-04-30 05:06:27 +0000

    Unlimited Color Gen

--- PROJECTS/Unlimited Colors/main.js
+++ PROJECTS/Unlimited Colors/main.js
@@ -1,17 +1,48 @@
 const body = document.querySelector("body")
 
 const colorGen = function() {
-    const randomColor = Math.round(Math.random() * 999999 + 1)
-    console.log(randomColor)
-    body.style.backgroundColor = `#${randomColor}`
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+
+    for(let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
 }
 
-document.querySelector("#start").addEventListener("click", function() {
-    console.log("clicked");
-    const interval = setInterval(colorGen, 1000)
+let intervalId;
+
+const startChanginhColor = function() {
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColor, 1000)
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = colorGen()
+    }
+}
+const stopChanginhColor = function() {
+    clearInterval(intervalId)
+    intervalId = null
+}
+
+document.querySelector("#start").addEventListener("click", startChanginhColor)
+document.querySelector("#stop").addEventListener("click", stopChanginhColor)
+
+
+// another method
+
+// const randomColor = Math.round(Math.random() * 999999 + 1)
+// console.log(randomColor)
+// body.style.backgroundColor = `#${randomColor}`
+// document.querySelector("#start").addEventListener("click", function() {
+//     const interval = setInterval(colorGen, 1000)
     
-    document.querySelector("#stop").addEventListener("click", function() {
-        console.log("stopped");
-        clearInterval(interval)
-    })
-})
+//     document.querySelector("#stop").addEventListener("click", function() {
+//         console.log("stopped");
+//         clearInterval(interval)
+//     })
+// })
+
+
+
