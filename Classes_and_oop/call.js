4fe96b87b666244d8dfc6b958f0bf6cca9d261fd
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-05-02 05:45:17 +0000

    Call Bind

---
+++ Classes_and_oop/call.js
@@ -0,0 +1,17 @@
+function SetUsername(username) {
+  this.username = username
+  console.log("called");  
+} 
+
+
+function createuser(username, email, password) {
+    
+    SetUsername.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+
+const chai = new createuser("chai", "chai@fb.com", 123)
+
+console.log(chai)
